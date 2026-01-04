aaeec10f18739dd3bc9371a0ab7d57633d0e4c6c
iqbalkdev <iqbalkresna.12@gmail.com>
2026-01-04 21:43:03 +0700

    feat: footer with company name

--- resources/js/layouts/components/Footer.vue
+++ resources/js/layouts/components/Footer.vue
@@ -4,13 +4,7 @@
     <span class="d-flex align-center text-medium-emphasis">
       &copy;
       {{ new Date().getFullYear() }}
-      Made
-      By <a
-        href="https://github.com/muhammadIqbalK"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="text-primary ms-1"
-      >MuhammadIqbalK</a>
+        PT SELARAS CITRA NUSANTARA PERKASA TBK.
     </span>
   </div>
 </template>
